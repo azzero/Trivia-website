6b1f44e584746f03dcc560004898152b49391063
mac <mac@MacBook-Pro-de-macbook.local>
2020-05-01 15:58:42 +0000

    add delete question endpoint

--- starter/backend/flaskr/__init__.py
+++ starter/backend/flaskr/__init__.py
@@ -62,11 +62,25 @@ def create_app(test_config=None):
         abort(404)
       return jsonify({
         'success':True,
-        "question":current_questions,
+        "questions":current_questions,
         "total_questions":len(Question.query.all()),
-        "categories":[ category.format()  for category in categories],
+        "categories": {categorie.id:categorie.type for categorie in categories},
         "current_category":None
       })
+  #TEST: At this point, when you start the application you should see questions and categories generated, ten questions per page and pagination at the bottom of the screen for three pages. Clicking on the page numbers should update the questions. 
+  #todo: Create an endpoint to DELETE question using a question ID. 
+    @app.route("/questions/<int:question_id>", methods=["DELETE"])
+    def delete_question(question_id):
+      try:
+
+        question=Question.query.filter(Question.id==question_id).one_or_none()
+        if (question is None ):
+          abort(404)
+        else:
+          question.delete()
+      except:
+        abort(422)
+
     return app
 
 
@@ -81,12 +95,9 @@ def create_app(test_config=None):
 
 
 
-  #TEST: At this point, when you start the application you should see questions and categories generated, ten questions per page and pagination at the bottom of the screen for three pages. Clicking on the page numbers should update the questions. 
   
 
 '''
-  @TODO: 
-  Create an endpoint to DELETE question using a question ID. 
 
   TEST: When you click the trash icon next to a question, the question will be removed.
   This removal will persist in the database and when you refresh the page. 
